79dd9baed705639a4d85cf29b894a00e669e1c63
Brian Holt <btholt@gmail.com>
2020-10-05 22:45:35 -0700

    initial commit :octocat:

---
+++ mongodb/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const connectionString =
+  process.env.MONGODB_CONNECTION_STRING || "mongodb://localhost:27017";
+
+async function init() {
+  const client = new MongoClient(connectionString, {
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+
+  const app = express();
+
+  app.get("/get", async (req, res) => {
+    console.log(req.query);
+
+    const db = await client.db("adoption");
+    const collection = db.collection("pets");
+
+    const pets = await collection
+      .find(
+        {
+          $text: { $search: req.query.search },
+        },
+        { _id: 0 }
+      )
+      .sort({ score: { $meta: "textScore" } })
+      .limit(10)
+      .toArray();
+
+    res.json({ status: "ok", pets }).end();
+  });
+
+  app.use(express.static("./static"));
+  app.listen(process.env.PORT || 3000);
+}
+init();
